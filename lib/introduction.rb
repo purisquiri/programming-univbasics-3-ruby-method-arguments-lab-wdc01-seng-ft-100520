61fe8a65cbe4bea7ac07f0ba68588490023a4dfe
Mauro Dorigo <mau.dorigo@outlook.es>
2020-08-19 17:53:25 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,6 +9,12 @@ end
 
 introduction("Mauro")
 
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."      
+end
+
+introduction_with_language("Mauro", "Ruby")
+
 
 
 
